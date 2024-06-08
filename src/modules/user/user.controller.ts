d8b5f25dbda2682e6c10c03388c2f789cc82b8da
Guilherme Akihito <akihitopro7@gmail.com>
2024-06-08 19:35:41 -0300

    usando interceptor para se familiarizar

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Put, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Put, Req, UseGuards, UseInterceptors } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { AuthGuard } from "src/common/guards/auth.guard";
 import { UserUpdateDTO } from "./dtos/user.update.DTO";
 import { UserId } from "src/common/decorators/user.id.decorator";
+import { LogInterceptor } from "src/common/interceptors/log.interceptor";
 
 @Controller("user")
 @UseGuards(AuthGuard)
+@UseInterceptors(LogInterceptor)
 export class UserController {
 
     constructor(private userService : UserService){}
-
+    
     @Get()
-    async getUser(@UserId() userId : number ){
+    async getUser(@UserId() userId : number ){    
         return this.userService.getUser(userId)
     }
 
@@ -20,8 +22,8 @@ export class UserController {
         return this.userService.updateUser(userId, body)
     }
 
-    @HttpCode(204) 
     @Delete()
+    @HttpCode(204) 
     async deleteUser(@UserId() userId : number){
         return this.userService.deleteUser(userId)
     }
